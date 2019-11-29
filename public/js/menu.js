aa52eaec41cde107b441d49fc713aed3a3f39ffc
Malo9663 <57362887+Malo9663@users.noreply.github.com>
2019-11-29 09:19:12 +0100

    Add files via upload
    JSON

---
+++ public/js/menu.js
@@ -0,0 +1,42 @@
+var food = [
+  {
+    "name" : "Big Kahuna Burger",
+    "kCal" : 750,
+    "lactose" : true,
+    "gluten" : true,
+    "ingriedents" : ["Nötfärs", "Ananas"],
+    "pic" : "img/burgare4.jpeg"
+  },
+  {
+    "name" : "Mäken Special",
+    "kCal" : 1050,
+    "lactose" : true,
+    "gluten" : true,
+    "ingriedents" : ["Älgfärs", "Bacon"],
+    "pic" : "img/Burger1.jpeg"
+  },
+  {
+    "name" : "Celsos Kyckling",
+    "kCal" : 850,
+    "lactose" : true,
+    "gluten" : true,
+    "ingriedents" : ["Kyckling", "Majonnäs"],
+    "pic" : "img/Burger2.jpg"
+  },
+  {
+    "name" : "Halloumiburgare",
+    "kCal" : 650,
+    "lactose" : false,
+    "gluten" : true,
+    "ingriedents" : ["Kyckling", "Majonnäs"],
+    "pic" : "img/Burger3.jpeg"
+  },
+  {
+    "name" : "Den Glutenfria",
+    "kCal" : 500,
+    "lactose" : false,
+    "gluten" : false,
+    "ingriedents" : ["Bönburgare", "Sallad"],
+    "pic" : "img/Burger5.jpeg"
+  }
+]
